989c5f8eebdbfa347f1e7799f6a7ff01d2e45a00
bmalkes <bmalkes@gmail.com>
2022-06-25 21:38:51 +0300

    push deploy

--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -19,6 +19,7 @@ const SinglePost = () => {
                 `*[slug.current == '${slug}']{
             title,
             id,
+            slug,
             mainImage{
                 asset->{
                     _id,url
