59bb984c308e447587464ea0b4ea04bf799175e0
bmalkes <bmalkes@gmail.com>
2022-07-02 14:37:16 +0300

    push

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,64 +3,64 @@ import { NavLink } from "react-router-dom";
 import { SocialIcon } from "react-social-icons";
 
 const NavBar = () => {
-  return (
-    <header className="bg-red-600">
-      <div className="container mx-auto flex  justify-between">
-        <nav className="flex">
-          <NavLink
-            to="/"
-            className="inflex-flex items-center py-7 px-3 mr-6 text-red-100 hover:text-blue-200 text-6xl font-bold cursive "
-            activeClassName="text-white"
-          >
-            Bmalkes
-          </NavLink>
-          <NavLink
-            to="/post"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-blue-300"
-          >
-            Blog Post
-          </NavLink>
-          <NavLink
-            to="/project"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-blue-300"
-          >
-            Projects
-          </NavLink>
-          <NavLink
-            to="/about"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-blue-300"
-          >
-            About Me!
-          </NavLink>
-        </nav>
-        <div className="inline-flex items-center py-3 ">
-          <SocialIcon
-            network="facebook"
-            url="https://www.facebook.com/bmalkes"
-            className="mr-4"
-            style={{ height: 60, width: 60 }}
-            target="_blank"
-            fgColor="#fff"
-          />
-          <SocialIcon
-            network="linkedin"
-            url="https://linkedin.com/in/bmalkes"
-            className="mr-4"
-            style={{ height: 60, width: 60 }}
-            target="_blank"
-            fgColor="#fff"
-          />
-          <SocialIcon
-            network="github"
-            url="https://github.com/BmAlkes"
-            style={{ height: 60, width: 60 }}
-            target="_blank"
-            fgColor="#fff"
-          />
-        </div>
-      </div>
-    </header>
-  );
+    return (
+        <header className="bg-red-600 container max-auto flex">
+            <div className="container mx-auto flex  justify-between">
+                <nav className="flex">
+                    <NavLink
+                        to="/"
+                        className="inflex-flex items-center py-7 px-3 mr-6 text-red-100 hover:text-blue-200 text-6xl font-bold cursive "
+                        activeClassName="text-white"
+                    >
+                        Bmalkes
+                    </NavLink>
+                    <NavLink
+                        to="/post"
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-blue-300"
+                    >
+                        Blog Post
+                    </NavLink>
+                    <NavLink
+                        to="/project"
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-blue-300"
+                    >
+                        Projects
+                    </NavLink>
+                    <NavLink
+                        to="/about"
+                        className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-blue-300"
+                    >
+                        About Me!
+                    </NavLink>
+                </nav>
+                <div className="inline-flex items-center py-3 ">
+                    <SocialIcon
+                        network="facebook"
+                        url="https://www.facebook.com/bmalkes"
+                        className="mr-4"
+                        style={{ height: 60, width: 60 }}
+                        target="_blank"
+                        fgColor="#fff"
+                    />
+                    <SocialIcon
+                        network="linkedin"
+                        url="https://linkedin.com/in/bmalkes"
+                        className="mr-4"
+                        style={{ height: 60, width: 60 }}
+                        target="_blank"
+                        fgColor="#fff"
+                    />
+                    <SocialIcon
+                        network="github"
+                        url="https://github.com/BmAlkes"
+                        style={{ height: 60, width: 60 }}
+                        target="_blank"
+                        fgColor="#fff"
+                    />
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default NavBar;
